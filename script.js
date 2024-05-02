f5c452f1ca16df432777e1409c2ba1df2b240107
Key Feula <keyfeula1214@gmail.com>
2024-05-01 18:28:16 -0700

    Add comment explaining number in for loop
    For loop goes from 0 to 255 because we need 256 total squares in the grid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridSquares = [];
 
+//Need 16x16 grid, which is 256 total grid squares
 for (let i = 0; i < 256; i++) {
     let gridSquare = document.createElement("div");
     gridSquare.classList.add("grid-square");
