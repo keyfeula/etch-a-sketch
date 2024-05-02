a380d5d55f46ab3181405abe3a901ad5dd720f9d
Key Feula <keyfeula1214@gmail.com>
2024-05-02 16:49:13 -0700

    Refactor code related to grid creation
    Need to dynamically create grid rather than hard code the grid squares' sizes, so made createGrid and
    createRow functions which make flex rows and then create a flex grid from the rows

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridSquares = [];
 
-//Need 16x16 grid, which is 256 total grid squares
-for (let i = 0; i < 256; i++) {
-    let gridSquare = document.createElement("div");
-    gridSquare.classList.add("grid-square");
-    gridSquares.push(gridSquare);
-    gridContainer.appendChild(gridSquare);
+function createGrid(sideLength) {
+    for (let i = 0; i < sideLength; i++) {
+        gridContainer.appendChild(createRow(sideLength));
+    }
+}
+
+function createRow(rowLength) {
+    let gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+    for (let i = 0; i < rowLength; i++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+        gridRow.appendChild(gridSquare);
+    }
+    return gridRow;
 }
 
 function mouseoverHandler(event) {
@@ -17,3 +26,5 @@ function mouseoverHandler(event) {
 }
 
 gridContainer.addEventListener("mouseover", mouseoverHandler);
+
+createGrid(20);
